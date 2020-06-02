660ba7297f31d9bf5700d0283665ab86570a1fe2
ggrelaxi <ggrelaxi@gmail.com>
2020-06-02 23:45:28 +0300

    add option -f

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -4,8 +4,13 @@ import commander from 'commander';
 
 commander
   .version('0.0.1')
-  .description('Compares two configuration files and shows a difference.');
-
-commander.parse(process.argv);
+  .description('Compares two configuration files and shows a difference.')
+  .option('-f, --format [type]', 'output format');
 
 // Commands
+
+commander
+  .arguments('<path1> <path2>')
+  .action(() => {});
+
+commander.parse(process.argv);
