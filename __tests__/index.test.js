850cad404efec780e7e7a4bd37ae363e2bf39e8a
ggrelaxi <ggrelaxi@gmail.com>
2020-06-09 20:33:55 +0300

    fix test file

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -7,6 +7,6 @@ import gendiff from '../index.js';
 test('gendiff', () => {
   const firstPath = 'before.json';
   const secondPath = 'after.json';
-  const correctResult = fs.readFileSync('/home/alex/Desktop/Git-repository/hexlet/frontend-project-lvl2/__tests__/fixtures/resultDifferent');
+  const correctResult = fs.readFileSync('./__tests__/fixtures/resultDifferent');
   expect(gendiff(firstPath, secondPath)).toEqual(correctResult.toString());
 });
